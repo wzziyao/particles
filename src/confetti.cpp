2d18fcae6c552993268208851950ff8c8b7f25b9
wzziyao <ziyaow36@gmail.com>
2021-04-29 01:27:24 -0400

    confetti, need to sort

--- src/confetti.cpp
+++ src/confetti.cpp
@@ -13,11 +13,35 @@ using namespace glm;
 void Confetti::createParticles(int size) 
 {
    mTexture = theRenderer.loadTexture("../textures/particle.png");
-   // TODO: Create particles
+   for (int i = 0; i < size; i++) {
+     Particle newParticle = {random_unit_cube(), random_unit_vector(), vec4(random_unit_vector(), 1.0), 0.25f, 1.0f};
+     mParticles.push_back(newParticle);
+   }
 }
 
 void Confetti::update(float dt)
 {
-  // update particles
+  for (int i = 0; i < mParticles.size(); i++) {
+    Particle current = mParticles[i];
+    vec3 pos = current.pos;
+    vec3 vel = current.vel;
+    if (pos.x <= -1 || pos.x >= 1) vel.x = -vel.x;
+    if (pos.y <= -1 || pos.y >= 1) vel.y = -vel.y;
+    if (pos.z <= -1 || pos.z >= 1) vel.z = -vel.z;
+    current.pos = pos + vel * dt;
+    current.vel = vel;
+    mParticles[i] = current;
+  }
+
+  vec3 camPos = theRenderer.cameraPosition();
+  for (int j = 1; j < mParticles.size(); j++) {
+    float d2 = length(camPos - mParticles[j].pos);
+    float d1 = length(camPos - mParticles[j-1].pos);
+    if (d2 < d1) {
+      Particle temp = mParticles[j];
+      mParticles[j] = mParticles[j-1];
+      mParticles[j-1] = temp;
+    }
+  }
 }
 
